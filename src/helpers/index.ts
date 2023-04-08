f0d21ac1a2cd971e4805ebd35accd114ee89320c
Racyn Ibaya <racynibaya@outlook.com>
2023-04-08 14:34:53 -0400

    authentication, authorization, create, read, update, delete, using ts in nodejs

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,9 +1,14 @@
 import crypto from 'crypto';
 
-import dotenv from 'dotenv'
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export const authentication = (salt: string, password: string): string => {
-  return crypto.createHmac('sha256', [salt, password].join('/')).update(process.env.SECRET).digest('hex');
-}
+  return crypto
+    .createHmac('sha256', [salt, password].join('/'))
+    .update(process.env.SECRET || 'RACYN-REST-API')
+    .digest('hex');
+};
 
 export const random = () => crypto.randomBytes(128).toString('base64');
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
-import dotenv from 'dotenv'
+import * as dotenv from 'dotenv';
 
 import mongoose from 'mongoose';
 import router from './router';
@@ -13,14 +13,15 @@ const app = express();
 
 dotenv.config();
 
-app.use(cors({
-  credentials: true,
-}));
+app.use(
+  cors({
+    credentials: true,
+  })
+);
 
 app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
-
 
 const server = http.createServer(app);
 
@@ -28,17 +29,11 @@ server.listen(9000, () => {
   console.log('Server running on http://localhost:9000/');
 });
 
-
-
-
-
 mongoose.Promise = Promise;
-mongoose.connect(process.env.MONGO_URL);
+mongoose.connect(
+  process.env.MONGO_URL ||
+    'mongodb+srv://racyn:racyn@cluster0.hsxf7z7.mongodb.net/?retryWrites=true&w=majority'
+);
 mongoose.connection.on('error', (error: Error) => console.log(error));
 
 app.use('/', router());
-
-app.get('/', (req, res) => {
-  res.send('Working')
-})
-
